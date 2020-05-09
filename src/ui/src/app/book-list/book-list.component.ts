f108306460709f9ef470ebd4547bf2968afeb412
Caio F. M. Cavalcanti <caiofabiomc@gmail.com>
2020-05-09 22:17:34 +0200

    adds ease in-out animation on mat cards

--- src/ui/src/app/book-list/book-list.component.ts
+++ src/ui/src/app/book-list/book-list.component.ts
@@ -2,11 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../models/book';
 import { BookService } from '../services/book.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
+  animations: [
+    trigger('scale', [
+      state('in', style({ transform: 'scale(1)' })),
+      transition(':enter', [
+        style({ transform: 'scale(0)' }),
+        animate('0.2s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.2s ease-out', style({ transform: 'scale(0)' }))
+      ]),
+    ])
+  ]
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
