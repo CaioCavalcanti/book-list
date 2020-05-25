278eeba0ca999a0ca5f96b4de5c3ae469e2c7fce
Caio F. M. Cavalcanti <caiofabiomc@gmail.com>
2020-05-25 21:28:02 +0200

    adds try catch on get books route

--- src/api/functions/get_books/app.py
+++ src/api/functions/get_books/app.py
@@ -3,10 +3,14 @@ import sys
 sys.path.append("/opt")
 
 from booklist import book_repository, book_mapper
-from booklist.utils import response_factory
+from booklist.utils import response_factory, logger
 
 
 def lambda_handler(event, context):
-    book_items = book_repository.get_books()
-    book_dtos = book_mapper.map_to_dto_list(book_items)
-    return response_factory.ok(book_dtos)
+    try:
+        book_items = book_repository.get_books()
+        book_dtos = book_mapper.map_to_dto_list(book_items)
+        return response_factory.ok(book_dtos)
+    except:
+        correlation_id = logger.log_error()
+        return response_factory.internal_server_error(correlation_id)
